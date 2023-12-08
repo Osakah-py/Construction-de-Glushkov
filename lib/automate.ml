710229ec19dec294803f739273127ae3b833884f
daniel.luo <daniel.luo@BC102-LIN-P05.TLPU066.etab.local>
2023-12-08 14:45:52 +0100

    test

--- lib/automate.ml
+++ lib/automate.ml
@@ -2,5 +2,16 @@ type automate = {
   nb_etats : int;
   initial : int;
   terminaux : bool array;
-  transitions : int array array
+  transitions : int array array (* x: etat || y: lettre *)
 }
+
+let creer_transition x a y aut =
+  aut.transitions.(x).(a) <- y;;
+
+let creer_automate_local n (pref: int list) (suff: int list) (fact: int list) =
+  let res =
+    {
+      nb_etats = n + 1; (* epsilon : -1 *)
+      initial = -1;
+      terminaux = Array.length n 
+    }
