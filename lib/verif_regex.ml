67bb0add29a7a8f11c92c00316651f9090f0fe59
alexandre.lescot <alexandre.lescot@BC102-LIN-P04.TLPU066.etab.local>
2023-12-08 14:58:18 +0100

    avance dans la verif

--- lib/verif_regex.ml
+++ lib/verif_regex.ml
@@ -6,9 +6,15 @@
       ? : optionel (zéro ou une fois)   
 *)
 
+let union (pile : char list) = match pile with 
+  |
+
+
+
 let reconnaitre_regex (str: string) = 
   let pile = ref [] in
   for i=0 to (String.length str) - 1 do
     if str.[i] = '!' then
+      let valid     
       Printf.printf("bouh !")
   done 
