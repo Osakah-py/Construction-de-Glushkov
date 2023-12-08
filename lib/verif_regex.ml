3c24ed53d17629a41b1ee98bfb94393dff6142c0
Osakah-py <alexandre.lescot@BC102-LIN-P04.TLPU066.etab.local>
2023-12-08 15:33:20 +0100

    avancée dans la verif

--- lib/verif_regex.ml
+++ lib/verif_regex.ml
@@ -11,14 +11,30 @@ exception InvalidRegex of string
 let union (pile : regex list) = match pile with 
   |[] -> raise (InvalidRegex "Union vide")
   |[a] -> raise (InvalidRegex "Union a un seul élément")
-  |a::b::t -> Union(a, b)
+  |a::b::t -> Ou(a, b)::t
 
+let inter (pile : regex list) = match pile with 
+  |[] -> raise (InvalidRegex "Intersection vide")
+  |[a] -> raise (InvalidRegex "Intersection a un seul élément")
+  |a::b::t -> Et(a, b)::t
 
+let kleene (pile : regex list) = match pile with 
+  |[] -> raise (InvalidRegex "Kleene tout nu !")
+  |a::t -> Kleene(a)::t
+
+let un_ou_rien (pile : regex list) = match pile with 
+  |[] -> raise (InvalidRegex "? est vide ????")
+  |a::t -> Ou(a, Eps)::t
 
 let reconnaitre_regex (str: string) = 
   let pile = ref [] in
   for i=0 to (String.length str) - 1 do
-    if str.[i] = '!' then
-      let valid     
-      Printf.printf("bouh !")
+    match str.[i] with
+    | '|' -> pile := union !pile
+    | '@' -> pile := inter !pile
+    | '*' -> pile := kleene !pile
+    | '?' -> pile := un_ou_rien !pile
+    
+      
+    end
   done 
