5b4ef0f5f34220901346ab2c1e6dadcc07d96abf
daniel.luo <daniel.luo@BC102-LIN-P05.TLPU066.etab.local>
2023-12-08 15:45:15 +0100

    zpgtjzrpg

--- lib/automate.ml
+++ lib/automate.ml
@@ -1,11 +1,14 @@
+open Type;;
+
 let creer_transition x a y aut =
   aut.transitions.(x).(a) <- y;;
 
 let creer_automate_local 
- (alphabet: char*int array) (pref: int list) (suff: int list) (fact: int list) =
-  let res =
+  n (pref: int list) (suff: int list) (fact: facteur list) = 
+ let res =
     {
       nb_etats = n + 1; (* epsilon : -1 *)
       initial = -1;
-      terminaux = Array.length n 
+      terminaux = Array.length n; 
     }
+
