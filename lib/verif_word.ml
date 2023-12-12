b38c4ced0515578fffb685ec6a0e8c7e875295a3
Osakah-py <lescot.alexg@mail.com>
2023-12-12 21:10:41 +0000

    début implem de Parcours Direct

--- lib/verif_word.ml
+++ lib/verif_word.ml
@@ -10,5 +10,10 @@ open Type;;
 (**********************)
 
 let str_in_anfd (str: string) (automate : afnd) =
-  if automate.transition.(automate.initial).(Char.code (str.[1])) = [] then 
-    Printf.printf "Vide"
+  let lgt = (String.length str) in
+  let aux (curr_char:int) (curr_state:int) =
+    if curr_char = lgt then
+       automate.terminaux.(curr_state)
+    else
+      List.iter (aux (curr_char+1)) (automate.transition.(automate.initial).(Char.code (str.[curr_char])))
+  in aux 0 automate.initial
